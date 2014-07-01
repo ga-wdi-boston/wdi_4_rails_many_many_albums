714def64d9a42e03664f65aaf4c66e76093b93fe
Tom Dyer <tdyer@ourstage.com>
2014-07-01 12:34:30 -0400

    Add a user and relationships between users and albums.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
+User.delete_all
 Album.delete_all
 Song.delete_all
+ManagedAlbums.delete_all
 
 sea_change = Album.create(name: "Sea Change", genre: 'rock')
 sea_change.songs.create!(title: 'Golden Age', artist: "Beck", price: 1.99, duration: 215)
@@ -12,3 +14,18 @@ nevermind.songs.create!(title: 'Lithium', artist: 'Nirvana', duration: 193, pric
 nevermind.songs.create!(title: 'Come as you are', artist: 'Nirvana', duration: 177, price: 1.49)
 
 
+jill =  User.create!(email: 'jill@example.com', password: 'password')
+fred =  User.create!(email: 'fred@example.com', password: 'password')
+mort =  User.create!(email: 'mort@example.com', password: 'password')
+tom =  User.create!(email: 'tom@example.com', password: 'password')
+
+mule = Album.create!(name: 'Mule Variations', genre: 'jazz')
+mule.songs.create!(title: 'Chocolate Jesus', artist: 'Tom Waits', duration: 133, price: 1.99)
+mule.songs.create!(title: 'Picture in a Frame', artist: 'Tom Waits', duration: 202, price: 3.99)
+
+mort.managed_albums.create!(album: nevermind, role: 'admin')
+mort.managed_albums.create!(album: sea_change, role: 'artist')
+jill.managed_albums.create!(album: nevermind, role: 'artist')
+jill.managed_albums.create!(album: sea_change, role: 'artist')
+fred.managed_albums.create!(album: nevermind, role: 'creative director')
+tom.managed_albums.create!(album: mule, role: 'artist')

7e03185b8f54cfc70e4297ca4099b86f5395dc25
Tom Dyer <tdyer@ourstage.com>
2014-07-01 12:33:11 -0400

    Add a has many through from users to albums

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Added a child relationship for managed albums.
+  # This user can have one or more managed albums.
+  has_many :managed_albums, class_name: 'ManagedAlbums', dependent: :destroy
+
+  # has many through
+  has_many :albums, through: :managed_albums
 end
